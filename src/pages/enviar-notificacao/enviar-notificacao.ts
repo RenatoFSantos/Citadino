92837ef4b7ef8d19b13e59e16ffcc8f022006748
marcusdurao <mvdurao@gmail.com>
2017-09-06 17:32:34 -0300

    Enviar Notificacao

---
+++ src/pages/enviar-notificacao/enviar-notificacao.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'page-enviar-notificacao',
+  templateUrl: 'enviar-notificacao.html',
+})
+export class EnviarNotificacaoPage {
+
+  constructor() {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad EnviarNotificacaoPage');
+  }
+
+}
