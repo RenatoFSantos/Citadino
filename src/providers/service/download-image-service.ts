9b56ce51303cc6db524e599952607ee08eabf325
marcusdurao <mvdurao@gmail.com>
2018-01-24 17:46:25 -0200

    Versao Cupom Desconto

--- src/providers/service/download-image-service.ts
+++ src/providers/service/download-image-service.ts
@@ -15,6 +15,8 @@ export class DownloadImageService {
     var urlImage: string = "";
     const fileTransfer: FileTransferObject = this.transfer.create();
 
+    console.log(" antes de fazer download " + this.glbVar.getStorageDirectory());
+
     return fileTransfer.download(url, this.glbVar.getStorageDirectory() + cupomKey + '.jpg');
   }
 
