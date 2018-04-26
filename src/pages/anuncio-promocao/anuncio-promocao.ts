04beda541163bf930abd7afb82c992c63e59f8af
Marcus Durao <mvdurao@gmail.com>
2018-04-26 13:52:59 -0300

    Versao Filtro Todos Municipios

--- src/pages/anuncio-promocao/anuncio-promocao.ts
+++ src/pages/anuncio-promocao/anuncio-promocao.ts
@@ -169,6 +169,7 @@ export class AnuncioPromocaoPage {
 
           updates['/vitrine/' + vitrine.muni_sq_id + '/' + vitrine.vitr_sq_id] = vitrine;
 
+          //TODOS MUNICIPIOS
           updates['/vitrine/' + self.glbVar.getMunicipioTodos().muni_sq_id + '/' + vitrine.vitr_sq_id] = vitrine;
 
           self.cupoCriaSrv.getDataBaseRef().update(updates).then(() => {
